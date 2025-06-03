84104c44c1da6e79f5c16a0c37e8e4f568729a0c
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-03 21:23:21 +0300

    0.1

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('No root element found');
+ReactDOM.createRoot(rootElement).render(<App />);
