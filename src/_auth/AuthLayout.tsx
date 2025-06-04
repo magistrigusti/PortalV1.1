a734ca531612f54d38e45e03ae741bcc46dac22e
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-04 17:45:11 +0300

    0.2.3

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -17,7 +17,7 @@ export default function AuthLayout() {
           </section>
 
           <img
-            src="/assets/images/bg-portal.png"
+            src="/assets/images/bg-p.jpg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
