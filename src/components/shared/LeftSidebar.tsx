971be8138c402484e344ebc1d1d34e915d87cf49
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-08 14:38:14 +0300

    0.4

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,9 +1,40 @@
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import { INavLink } from "@/types";
+import { sidebarLinks } from "@/constants";
+import { Loader } from "@/components/shared";
+import { Button } from "@/components/ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queries";
+import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
 
 const LeftSidebar = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user, setUser, setAuthenticated, isLoading } = useUserContext();
+  const { mutate: signOut } = useSignOutAccount();
+
+  const handleSignOut = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    signOut();
+    setAuthenticated(false);
+    setUser(INITIAL_USER);
+    navigate("sign-in");
+  };
+
   return (
-    <div>
-      LeftSidebar
-    </div>
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img 
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,13 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
 import { Button } from "../ui/button";
+import { useUserContext } from "@/context/AuthContext";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
-import { useUserContext } from '@/context/AuthContext';
 
 const Topbar = () => {
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
   const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
 
   useEffect(() => {
     if (isSuccess) navigate(0);
@@ -17,35 +18,32 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img 
-            src='/assets/images/logo.png'
-            alt=""
+          <img
+            src="/assets/images/logo.png"
+            alt="logo"
             width={130}
+            height={325}
           />
         </Link>
 
         <div className="flex gap-4">
-          <Button className="shad-button_ghost"
+          <Button
             variant="ghost"
-            onClick={() => signOut()}
-          >
-            <img src="/assets/icons/logout.svg" 
-              alt="logout"
-            />
+            className="shad-button_ghost"
+            onClick={() => signOut()}>
+            <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
-
-          <Link className="flex-center gap-3"
-            to={`/profile/${user.id}`}
-          >
-          <img className="h-8 w-8 rounded-full"
-            src={user.imageUrl || 'assets/images/profileplaceholder.svg'} 
-            alt="profile"
-          />
-          </Link >
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Topbar;
