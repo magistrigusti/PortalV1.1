339439935160c0f134e12a910f448feb3c1df38b
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-11 10:02:15 +0300

    0.5.6

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -70,7 +70,7 @@ const PostCard = ({ post }: PostCardProps) => {
         </div>
 
         <img
-          src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
+          src={post.imageUrl }
           alt="post image"
           className="post-card_img"
         />
